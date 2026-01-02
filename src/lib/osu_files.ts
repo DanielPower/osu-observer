0e743029d343b02ba80a7c1c2d686c13ac23a529
Daniel Power <me@danielpower.ca>
2026-01-02 03:58:10 -0330

    Maybe fix, idfk anymore

--- src/lib/osu_files.ts
+++ src/lib/osu_files.ts
@@ -1,22 +1,7 @@
-import { BlobWriter } from '@zip.js/zip.js';
 import { BeatmapDecoder, ScoreDecoder } from 'osu-parsers';
 
 const beatmapDecoder = new BeatmapDecoder();
 const scoreDecoder = new ScoreDecoder();
-
-export const extractAudioFile = async (entry: any) => {
-	if (!entry.getData) {
-		throw new Error('Entry does not have a getData method');
-	}
-
-	const blobWriter = new BlobWriter();
-	await entry.getData(blobWriter);
-	const blob = await blobWriter.getData();
-
-	// Create an object URL for the audio blob
-	const audioUrl = URL.createObjectURL(blob);
-	return audioUrl;
-};
 
 export const readBeatmap = async (url: string) => {
 	const response = await fetch(url);
