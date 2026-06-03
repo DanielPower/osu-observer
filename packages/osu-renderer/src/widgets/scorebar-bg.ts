4d44ebb468078f9644ac78cb702d5e8d938880d2
Daniel Power <me@danielpower.ca>
2026-06-02 23:57:58 -0230

    Fix scorebar sizing

--- packages/osu-renderer/src/widgets/scorebar-bg.ts
+++ packages/osu-renderer/src/widgets/scorebar-bg.ts
@@ -1,14 +1,13 @@
 import type { WidgetFactory } from "./widget";
-import { drawSprite } from "../renderer/draw";
 
 export const scorebarBgWidget: WidgetFactory = (widgetContext) => {
   return {
     draw(ctx) {
       const image = widgetContext.images["scorebar-bg"];
       if (!image) return;
-      const scorebarScale = widgetContext.width / image.width;
-      const height = image.height * scorebarScale;
-      drawSprite(ctx, image, widgetContext.width / 2, height / 2, widgetContext.width, height);
+
+      const scale = widgetContext.height / 768;
+      ctx.drawImage(image, 0, 0, image.width * scale, image.height * scale);
     },
   };
 };
